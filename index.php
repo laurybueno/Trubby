129b7f1171fc94b33fdb1bce189535eaed9b9e79
Lucas Delboni <lucas_cd460@hotmail.com>
2015-09-25 20:31:20 +0000

    quebrando o index de verdade para testar o selenium

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
             <div class="container-fluid">
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li><a href="estoque/mostraestoque.php">Estoque</a></li>
+                        
                         <!-- <li><a href="estoque/mostraestoque.php">Estoque</a></li> -->
                         <li><a href="#">Receitas</a></li>
                         <li><a href="#">Caixa</a></li>
