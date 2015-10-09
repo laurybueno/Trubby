e996bfae932cce11ae98b7806b6077e1385993a2
Lucas Delboni <lucas_cd460@hotmail.com>
2015-10-09 05:02:51 +0000

    feito o mostra receitas

--- receitas/mostraReceita.php
+++ receitas/mostraReceita.php
@@ -1,85 +1,111 @@
-<?php
-    session_start();
-?>
 <!DOCTYPE html>
 <html>
     <head>
         <?php
-            include "../bootstrap.php" 
+        include "../valida_session.inc.php";
+            
+            if($login === false){
+                header("Location: ../usuario/naoLogado.php");
+            }
+            include "../bootstrap.php";
+            include "insereReceita.php";
         ?>
-        
         <script src="../1.RESOURCES/jquery-1.11.3.min.js"></script>
         <script src="../1.RESOURCES/bootstrap/js/bootstrap.min.js"></script>
         <script src="../1.RESOURCES/jquery.tablesorter.min.js"></script>
     </head>
     <body>
-        <script type="text/javascript" >
-            $(document).ready(function() { 
-                $("#tabelaReceita").tablesorter(); 
-            }); 
-        </script>
-
         <div class="container">
-            <div>
-                <h3>
-                    <?php
-                        echo  $_SESSION["nomeItem"]. " " . $_SESSION["erro"] . "<br>";
-                        session_destroy(); 
-                    ?>
-                </h3>
+            <a href="../index.php" class="btn btn-default"><span aria-hidden="true">&larr;</span> Voltar</a>
+             <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        
+                        <li><a href="estoque/mostraestoque.php">Estoque</a></li>
+                        <li><a href="receitas/mostraReceita.php">Receitas</a></li>
+                        <li><a href="#">Caixa</a></li>
+                        <li><a href="#">Cardápio</a></li>
+                    </ul>
+                </div>
             </div>
-                <!-- script para ordenar a tabela -->
-                <script type="text/javascript" >
-                    $(document).ready(function() { 
-                        $("#tabelaReceita").tablesorter(); 
-                    }); 
-                </script>
             
-                <!-- script para linkar a linha da tabela-->
-                <script type="text/javascript">
-                    jQuery(document).ready(function($) {
-                        $(".clickable-row").click(function() {
-                            window.document.location = $(this).data("href");
+    
+            <div class="container">
+                <div>
+                    <h3>
+                        <?php
+                            echo  $_SESSION["nomeItem"]. " " . $_SESSION["erro"] . "<br>";
+                        ?>
+                    </h3>
+                </div>
+                    <!-- script para ordenar a tabela -->
+                    <script type="text/javascript" >
+                        $(document).ready(function() { 
+                            $("#tabelaReceita").tablesorter(); 
+                        }); 
+                    </script>
+                
+                    <!-- script para linkar a linha da tabela-->
+                    <script type="text/javascript">
+                        jQuery(document).ready(function($) {
+                            $(".clickable-row").click(function() {
+                                window.document.location = $(this).data("href");
+                            });
                         });
-                    });
-                </script>
-                <?php
-                    require_once("../connection.php");
-                    
-                    $sql = "SELECT `id_ficha`,`nome`, `foto` FROM `fichas`";
-                    $resultado = mysql_query($sql);
-                    mysql_close($con);
-                    
-
-                    echo 
-                        '<table class="table table-striped" id="tabelaReceita">
-                            <thead>
-                                <tr>
-                                    <td> <strong>Nome da receita</strong> </td>
-                                    <td> <strong>Foto</strong> </td>
-                                </tr>
-                            </thead>
-                            <tbody>';
-                                while($linha = mysql_fetch_array($resultado)){
-                                    echo
-                                    #isso nao é seguro (ta mandando um get com o id da receita)
-                                        '<tr  class="clickable-row" data-href="/maisInformacoes?receita=' .$linha['id_ficha'] .'"> 
-                                            <td>
-                                                ' .$linha['nome'] .'
-                                            </td>
-                                            <td>
-                                                ' .$linha['foto'] .'
-                                            </td>
-                                            
-                                        </tr>
-                                        ';
-                                }
-                ?>
-                            </tbody>
-                        </table>
-            
-            <!-- Trigger the modal with a button -->
-            <a href="/insereReceita.php" class="btn btn-info btn-lg"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Inserir nova ficha técnica</a>
+                    </script>
+                    <?php
+                        require_once("../connection.php");
+                        
+                        $sql = "SELECT `id_ficha`,`nome`, `foto` FROM `fichas`";
+                        $resultado = mysql_query($sql);
+                        mysql_close($con);
+                        
+    
+                        echo 
+                            '<table class="table table-striped" id="tabelaReceita">
+                                <thead>
+                                    <tr>
+                                        <td> <strong>Nome da receita</strong> </td>
+                                        <td> <strong>Foto</strong> </td>
+                                    </tr>
+                                </thead>
+                                <tbody>';
+                                    while($linha = mysql_fetch_array($resultado)){
+                                        echo
+                                        #isso nao é seguro (ta mandando um get com o id da receita)
+                                            '<tr> 
+                                                <td>
+                                                    ' .$linha['nome'] .'
+                                                </td>
+                                                <td>
+                                                    ' .$linha['foto'] .'
+                                                </td>
+                                                <td>
+                                                    <form class="form-horizontal"  action="" role="form" method="POST">
+                                                        <button type="button" data-id="'.$linha['id_ficha'].'" data-nome="'.$linha['nome'].'" data-foto="'.$linha['foto'].'" class="open-ModificaDialog btn btn-info btn-lg" data-toggle="modal" data-target="#modificarItemReceita">
+                                                            <span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Modificar
+                                                        </button>
+                                                     </form>
+                                                </td>
+                                                <td>
+                                                    <form class="form-horizontal"  action="" role="form" method="POST">
+                                                        <button type="button" data-id="'.$linha['id_ficha'].'" class="open-DeletaDialog btn btn-info btn-lg" data-toggle="modal" data-target="#deletarItemReceita">
+                                                            <span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Deletar
+                                                        </button>
+                                                    </form>
+                                                </td>
+                                            </tr>
+                                            ';
+                                    }
+                    ?>
+                                </tbody>
+                            </table>
+                
+                <!-- Trigger the modal with a button -->
+                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#insereReceita">
+                   <span aria-hidden="true"></span> Inserir nova ficha técnica
+                </button>
+            </div>
         </div>
     </body>
 </html>
