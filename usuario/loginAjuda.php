7a8442374d3ef2f17cdb3756c7e7c42a7adeeaec
Lucas Delboni <lucas_cd460@hotmail.com>
2015-10-09 20:26:56 +0000

    removido a imagem de ajuda

--- usuario/loginAjuda.php
+++ usuario/loginAjuda.php
@@ -3,7 +3,6 @@
         
     </head>
     <body>
-        <img class="foda-se" src="http://i.ytimg.com/vi/QLOY6s53-5k/hqdefault.jpg"alt="">
         <h1> Desculpe, pagina em construcao</h1>
     </body>
 </html>
