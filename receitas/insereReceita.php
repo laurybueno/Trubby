09de395df22ab2803f168cea66f51964c615cf11
Lucas Delboni <lucas_cd460@hotmail.com>
2015-10-12 01:12:11 +0000

    feito o dropdown com ingredientes do estoque

--- receitas/insereReceita.php
+++ receitas/insereReceita.php
@@ -1,16 +1,12 @@
 <?php
     include "../bootstrap.php";
-    include "insereIngrediente.php"
+    include "insereIngrediente.php";
+    
+
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <?php
-        include "../valida_session.inc.php";
-            if($login === false){
-                header("Location: ../usuario/naoLogado.php");
-            }
-        ?>
         <script src="../1.RESOURCES/jquery-1.11.3.min.js"></script>
         <script src="../1.RESOURCES/bootstrap/js/bootstrap.min.js"></script>
         <script src="../1.RESOURCES/jquery.tablesorter.min.js"></script>
@@ -37,25 +33,25 @@
                         <div class="form-group">
                             <label class="control-label col-sm-3" for="nomeItem">Nome da ficha técnica*:</label>
                             <div class="col-sm-7">
-                                <input type="text" class="form-control" id="nomeItem" name="nomeItem" placeholder="Ex.:crepe de morango" value='<?php echo htmlentities($nomeItem)?>'>
+                                <input type="text" class="form-control" id="nomeItem" name="nomeItem" placeholder="Ex.:crepe de morango" >
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label col-sm-3" for="nPorcoes">Número de porções*:</label>
                             <div class="col-sm-7">
-                                <input type="number" class="form-control" id="nPorcoes" name="nPorcoes" placeholder="Ex.: 2" value='<?php echo htmlentities($nPorcoes)?>'>
+                                <input type="number" class="form-control" id="nPorcoes" name="nPorcoes" placeholder="Ex.: 2" >
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label col-sm-3" for="pesoPorcao">Peso da porção (g)*:</label>
                             <div class="col-sm-7">
-                                <input type="number" class="form-control" id="pesoPorcao" name="pesoPorcao" placeholder="peso da porção em gramas Ex.: 2" value='<?php echo htmlentities($nPorcoes)?>'>
+                                <input type="number" class="form-control" id="pesoPorcao" name="pesoPorcao" placeholder="peso da porção em gramas Ex.: 2" >
                             </div>
                         </div>
                         <div class="form-group">
                             <label class="control-label col-sm-3" for="precoVenda">Preço de venda*:</label>
                             <div class="col-sm-7">
-                                <input type="number" class="form-control" id="precoVenda" name="precoVenda" placeholder="preço de venda em reais Ex.: 2,50" value='<?php echo htmlentities($nPorcoes)?>'>
+                                <input type="number" class="form-control" id="precoVenda" name="precoVenda" placeholder="preço de venda em reais Ex.: 2,50" >
                             </div>
                         </div>
                         <h2>Ingredientes:</h2>
@@ -68,13 +64,13 @@
                         <h4>Ingredientes secundários</h4>
                         <div class="form-group">        
                             <div align="center">
-                                <button id="submit" name="submitted" type="submit" value="Send" class="btn glyphicon glyphicon-plus btn-info"></button>
+                                <button id="submit" name="submitted" type="submit" value="Send" class="btn glyphicon glyphicon-plus btn-info" aria-hidden="true" data-toggle="modal" data-target="#inserirIngrediente"></button>
                             </div>
                         </div>
                         <h4>Ingredientes extras</h4>
                         <div class="form-group">        
                             <div align="center">
-                                <button id="submit" name="submitted" type="submit" value="Send" class="btn glyphicon glyphicon-plus btn-info"></button>
+                                <button id="submit" name="submitted" type="submit" value="Send" class="btn glyphicon glyphicon-plus btn-info" aria-hidden="true" data-toggle="modal" data-target="#inserirIngrediente"></button>
                             </div>
                         </div>
                         
@@ -83,12 +79,12 @@
                         <div class="form-group">
                             <label class="control-label col-sm-3" for="obs">Observações:</label>
                             <div class="col-sm-7">
-                                <input type="text" class="form-control" id="obs" name="obs" value='<?php echo htmlentities($nomeItem)?>'>
+                                <input type="text" class="form-control" id="obs" name="obs" >
                             </div>
                         </div>
                         <div class="form-group">        
                             <div align="center">
-                                <button id="submit" name="submitted" type="submit" value="Send" class="btn btn-primary">Confirmar</button>
+                                <button id="submit" name="submitted" type="submit" value="Send" class="btn btn-primary">Inserir receita</button>
                             </div>
                         </div>
                     </form>
