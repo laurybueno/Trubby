cf8715b5fbe80a922390d89f6e80e08b6ef87ef5
Lucas Delboni <lucas_cd460@hotmail.com>
2015-10-23 18:28:40 +0000

    funcoes que nao serao descontinuadas colocadas mas em comentario

--- connection.php
+++ connection.php
@@ -5,14 +5,17 @@
     // Press the 'Run' button on the top to start the web server,
     // then click the URL that is emitted to the Output tab of the console.
 
-    $servername = "54.94.128.189";
+    $servername = "54.207.22.190";
     $login = "trubby";
     $senhaDB = "raiztrubby";
     $database = "trubby";
     $dbport = 3306;
 
+    //$mysqli = new mysqli($servername, $login, $senhaDB, $database);
+    //if (mysqli_connect_errno()) 
+    //    trigger_error(mysqli_connect_error());
     $con = mysql_connect($servername, $login, $senhaDB) or
-        die('Não foi possível conectar');
+        die('Erro recebido do servidor MySQL: '.mysql_error());
     mysql_select_db($database, $con);
     /*$con = mysql_connect(getenv('IP'), getenv('C9_USER'), 'trubby') or
                         die('Não foi possível conectar');
